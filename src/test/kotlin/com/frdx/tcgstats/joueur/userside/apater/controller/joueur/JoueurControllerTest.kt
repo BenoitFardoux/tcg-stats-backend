31dae67731e1d92cbd95e5361f76eaed9205912f
Benoit Fardoux <86200150+BenoitFardoux@users.noreply.github.com>
2025-01-13 22:39:07 +0100

    feat(joueur) : suppression d'un joueur

--- src/test/kotlin/com/frdx/tcgstats/joueur/userside/apater/controller/joueur/JoueurControllerTest.kt
+++ src/test/kotlin/com/frdx/tcgstats/joueur/userside/apater/controller/joueur/JoueurControllerTest.kt
@@ -1,9 +1,16 @@
 package com.frdx.tcgstats.joueur.userside.apater.controller.joueur
 
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import com.frdx.tcgstats.joueur.serverside.adapter.mysql.repository.MariaDbJoueurRepository
+import com.frdx.tcgstats.joueur.serverside.mapper.JoueurMapper.toJoueurDocument
+import com.frdx.tcgstats.joueur.userside.apater.controller.joueur.joueurFixture.courrielInvalide
+import com.frdx.tcgstats.joueur.userside.apater.controller.joueur.joueurFixture.creerJoueurDTO
 import com.frdx.tcgstats.joueur.userside.apater.controller.joueur.joueurFixture.motDePasseInvalide
 import com.frdx.tcgstats.joueur.userside.apater.controller.joueur.joueurFixture.motDePasseValide
 import com.frdx.tcgstats.joueur.userside.dto.CreerJoueurRestRessource
+import com.frdx.tcgstats.joueur.userside.mapper.JoueurMapper.toJoueur
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
 import org.mockito.Mockito.`when`
@@ -16,7 +23,10 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.test.context.TestPropertySource
 import org.springframework.test.context.bean.override.mockito.MockitoBean
 import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.delete
+import org.springframework.test.web.servlet.get
 import org.springframework.test.web.servlet.post
+import java.util.*
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -27,10 +37,16 @@ class JoueurControllerTest {
     @Autowired
     private lateinit var mockMvc: MockMvc
 
+    @Autowired
+    private lateinit var mariaDbJoueurRepository: MariaDbJoueurRepository
 
     @MockitoBean
     private lateinit var bCryptPasswordEncoder: BCryptPasswordEncoder
 
+    @BeforeEach
+    fun setUp() {
+        mariaDbJoueurRepository.deleteAll()
+    }
 
 
     @Test
@@ -56,8 +72,8 @@ class JoueurControllerTest {
     @Test
     fun `Lorsque j'essaye de créer un joueur avec une adresse mail invalide, j'ai une erreur 400`() {
         // GIVEN
-        val password = motDePasseValide
-        val utilisateurACreer = CreerJoueurRestRessource("addresseMail", password, "test")
+        val utilisateurACreer = creerJoueurDTO.copy(courriel = courrielInvalide)
+        val password = utilisateurACreer.motDePasse
 
         // WHEN
         `when`(bCryptPasswordEncoder.encode(password)).thenReturn("#password")
@@ -76,7 +92,7 @@ class JoueurControllerTest {
     @Test
     fun `Lorsque j'essaye de créer un utilisateur avec un mot de passe invalide , j'ai une erreur 400 `() {
         // GIVEN
-        val utilisateurACreer = CreerJoueurRestRessource("addresseMail@mail.com", motDePasseInvalide, "test")
+        val utilisateurACreer = creerJoueurDTO.copy(motDePasse = motDePasseInvalide)
 
         // WHEN
         // THEN
@@ -88,5 +104,86 @@ class JoueurControllerTest {
             status { isBadRequest() }
             content { contentType(MediaType.APPLICATION_PROBLEM_JSON) }
         }
+    }
+
+    @Test
+    fun `Lorsque j'essaye de récuperer un utilisateur qui n'est pas en base de données je reçois une 404 not found`() {
+        // GIVEN
+        val uuidInnexistant = UUID.randomUUID()
+        // WHEN
+        //THEN
+        mockMvc.get("/tcgstat/joueur/${uuidInnexistant}") {
+            contentType = MediaType.APPLICATION_JSON
+            accept = MediaType.APPLICATION_JSON
+        }.andExpect {
+            status { isNotFound() }
+            content { contentType(MediaType.APPLICATION_PROBLEM_JSON) }
+        }
+    }
+
+    @Test
+    fun `Lorsque j'essaye de récuperer un utilisateur qui est  en base de données je reçois une 200`() {
+        // GIVEN
+        val user = mariaDbJoueurRepository.saveAndFlush(creerJoueurDTO.toJoueur().toJoueurDocument())
+
+        // WHEN
+        //THEN
+        mockMvc.get("/tcgstat/joueur/${user.id}") {
+            contentType = MediaType.APPLICATION_JSON
+            accept = MediaType.APPLICATION_JSON
+        }.andExpect {
+            status { isOk() }
+            content { contentType(MediaType.APPLICATION_JSON) }
+        }
+    }
+
+    @Test
+    fun `Lorsque j'esaye de récuperer la liste des utilisateurs, celle ci contient les utilisateurs de la base de données`() {
+        // GIVEN
+        mariaDbJoueurRepository.saveAndFlush(creerJoueurDTO.toJoueur().toJoueurDocument())
+        // WHEN
+
+        //THEN
+        mockMvc.get("/tcgstat/joueur") {
+            contentType = MediaType.APPLICATION_JSON
+            accept = MediaType.APPLICATION_JSON
+        }.andExpect {
+            status { isOk() }
+            content { contentType(MediaType.APPLICATION_JSON) }
+        }
+    }
+
+    @Test
+    fun `Lorsque j'essaye de supprimer un utilisateur et qu'il existe, celui ci est correctement supprimé`() {
+        // GIVEN
+        val joueur = mariaDbJoueurRepository.saveAndFlush(creerJoueurDTO.toJoueur().toJoueurDocument())
+        // WHEN
+        // THEN
+        mockMvc.delete("/tcgstat/joueur/${joueur.id}") {
+            contentType = MediaType.APPLICATION_JSON
+            accept = MediaType.APPLICATION_JSON
+        }
+
+            .andExpect {
+                status { isOk() }
+            }
+        assertThat(mariaDbJoueurRepository.existsById(joueur.id!!)).isFalse()
+    }
+
+    @Test
+    fun `Lorsque j'essaye de supprimer un utilisateur et qu'il n'existe pas, il y a une erreur 404`() {
+        // GIVEN
+        val idJoueur = UUID.randomUUID()
+        // WHEN
+        // THEN
+        mockMvc.delete("/tcgstat/joueur/${idJoueur}") {
+            contentType = MediaType.APPLICATION_JSON
+            accept = MediaType.APPLICATION_JSON
+        }
+
+            .andExpect {
+                status { isNotFound() }
+                content { contentType(MediaType.APPLICATION_PROBLEM_JSON) }
+            }
     }
 }
--- src/test/kotlin/com/frdx/tcgstats/joueur/userside/apater/controller/joueur/joueurFixture.kt
+++ src/test/kotlin/com/frdx/tcgstats/joueur/userside/apater/controller/joueur/joueurFixture.kt
@@ -1,6 +1,16 @@
 package com.frdx.tcgstats.joueur.userside.apater.controller.joueur
 
+import com.frdx.tcgstats.joueur.userside.dto.CreerJoueurRestRessource
+
 object joueurFixture {
+    const val courrielValdide = "addresseMail@mail.com"
+    const val courrielInvalide = "amail.co"
+    const val pseudoValide = "Joueur"
     const val motDePasseValide = "#Password12"
     const val motDePasseInvalide = "password"
+    val creerJoueurDTO = CreerJoueurRestRessource(
+        courrielValdide,
+        motDePasseValide,
+        pseudoValide
+    )
 }
