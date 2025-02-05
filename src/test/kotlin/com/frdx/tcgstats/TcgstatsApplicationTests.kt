a3f2db0f76b4784dc1a398483b0a6ad206bc4218
Benoit Fardoux <86200150+BenoitFardoux@users.noreply.github.com>
2025-02-05 21:33:19 +0100

    feat(test) : ajout testt

--- src/test/kotlin/com/frdx/tcgstats/TcgstatsApplicationTests.kt
+++ src/test/kotlin/com/frdx/tcgstats/TcgstatsApplicationTests.kt
@@ -2,12 +2,26 @@ package com.frdx.tcgstats
 
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
+import java.sql.Connection
+import java.sql.DriverManager
+import org.junit.jupiter.api.Assertions.assertNotNull
 
 @SpringBootTest
 class TcgstatsApplicationTests {
 
 	@Test
 	fun contextLoads() {
+	}
+
+	@Test
+	fun `test database connection`() {
+		val url = "jdbc:mariadb://mariadb:3306/statistiques_mtg_test"
+		val user = "root"
+		val password = "password"
+
+		val connection: Connection? = DriverManager.getConnection(url, user, password)
+		assertNotNull(connection, "La connexion à la base de données a échoué")
+		connection?.close()
 	}
 
 }
